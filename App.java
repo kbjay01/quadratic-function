1def6524eb27905b4c13cb327ad96ec2ef619732
Jakub <39642297+kbjay01@users.noreply.github.com>
2018-12-09 09:50:23 +0100

    Update App.java
    * removed checking conv_error
    * added callout to printer() in Calc.java

--- App.java
+++ App.java
@@ -1,14 +1,13 @@
 public class App {
     public static void main(String[] args){
-        Config Calculations = new Config();
+        Calc Calculations = new Calc();
+
         Calculations.intro();
         Calculations.variableInput();
         Calculations.showVariables();
         Calculations.convertVariables();
 
-        if(Calculations.convError == true){
-            Calculations.partOne();
-        }
         Calculations.counting();
+        Calculations.printer();
     }
 }
